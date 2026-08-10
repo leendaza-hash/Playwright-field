19f6c559621b4448db59aa6659d4a870c5539831
unknown <leenda.za@gmail.com>
2026-08-10 14:35:16 +0800

    Add comments

--- src/fixtures/auth.fixture.ts
+++ src/fixtures/auth.fixture.ts
@@ -1,5 +1,7 @@
 import {test as base} from '@playwright/test';
 
+
+//To create a new test with custom fixtures, we can use the base test and extend it with our own fixtures. In this case, we are creating a fixture for login functionality.
 type MyFixtures = {
   login: (username: string, password: string) => Promise<void>;
 };
