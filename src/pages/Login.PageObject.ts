7e135ba9e41c2318752b095e03477c1ffc8f0bc8
unknown <leenda.za@gmail.com>
2026-09-06 22:28:13 +0800

    commented loginTitle locator in login page object file

--- src/pages/Login.PageObject.ts
+++ src/pages/Login.PageObject.ts
@@ -1,7 +1,7 @@
 import { type Page, type Locator } from '@playwright/test';
 
 export class LoginPage {
-    public readonly loginTitle: Locator;
+    //public readonly loginTitle: Locator;
     public readonly googleButton: Locator;
     public readonly emailFieldTitle: Locator;
     //public readonly emailFieldPlaceholder: Locator;
@@ -20,7 +20,7 @@ export class LoginPage {
 
 
 constructor(private readonly page: Page) {
-    this.loginTitle = page.getByRole('heading', { name: 'Login' })
+    //this.loginTitle = page.getByRole('heading', { name: 'Login' })
     this.googleButton = page.getByRole("button", { name: 'Sign in with Google' });
     this.emailFieldTitle = page.getByText('Email address *');
     //this.emailFieldPlaceholder = page.getByPlaceholder('Your email');
